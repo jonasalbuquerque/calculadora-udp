a768dbc9d94ffc56dc8f30c263956f2841834885
jonasalbuquerque <jonasalbuquerque@alu.ufc.br>
2021-02-11 15:43:02 -0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/calculadoraUDP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculadoraUDP.dir/main.cpp.o"
+  "calculadoraUDP"
+  "calculadoraUDP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calculadoraUDP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
